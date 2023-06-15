abe838b85cca6fe637d2410c3126dcc327cfe904
Anand930singh <anand2002king@gmail.com>
2023-06-15 18:06:09 +0530

    Cors added2

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const bodyParser = require('body-parser');
 const PORT =process.env.PORT||5000
 const cors = require("cors");
 
-app.use(cors());
+
 
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 connectDb();
 
